b3cd7e6ae8c3cbfd213d648df6a694614054bc77
Vita Khutorianska <steblvita@gmail.com>
2021-04-05 16:09:31 +0300

    add short id

--- src/App.js
+++ src/App.js
@@ -1,7 +1,55 @@
+import { Component } from 'react';
 import './App.css';
+import shortid from 'shortid';
+import ContactForm from './Components/ContactForm';
+import ContactList from './Components/ContactList';
+import Filter from './Components/Filter';
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+  addContact = ({ name, number }) => {
+    // console.log("this.state.contacts",this.state.contacts)
+    const sameName = this.state.contacts.some(item => item.name === name);
+    console.log('sameName', sameName);
+
+    if (sameName) {
+      window.alert(`LocalHost:3000 says ${name} is already in contact`);
+      return;
+    }
+    let contact = {
+      name,
+      number: number,
+      id: shortid.generate(),
+    };
+
+    this.setState(preState => {
+      return {
+        contacts: [contact, ...preState.contacts],
+      };
+    });
+  };
+  render() {
+    const { name, filter } = this.state;
+    const normalizedFilter = this.state.filter.toLowerCase();
+    const filterNamePhoneBook = this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter),
+    );
+
+    return (
+      <div>
+        <ContactForm value={name} onSubmit={this.addContact} />
+
+        <Filter value={filter} onChange={this.filterChange} />
+        <ContactList
+          onDeleteContact={this.deleteContact}
+          contacts={filterNamePhoneBook}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
